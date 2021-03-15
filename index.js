eddb009873ddd2b14d40f4a422b6e50e39109dc6
faizahmaddae <faizahmad.daiee@gmail.com>
2021-03-15 10:31:11 +0430

    dd

--- index.js
+++ index.js
@@ -13,6 +13,13 @@ app.get('/about', (req, res) => {
   res.send(data)
 })
 
+
+app.get('/posts', (req, res) => {
+  res.send('<h1><a href="/">Home</a> ipsum dolor sit amet consectetur adipisicing elit. Autem aliquam, atque voluptate totam! Distinctio cum dolorem, aperiam doloribus eveniet nesciunt temporibus, nemo aspernatur! Quaerat suscipit sunt ipsa cumque omnis ea?</h1></h1>\
+\
+ <p>Lorem, ipsum dolor sit amet consectetur adipisicing, elit. Iure at fugiat aperiam laborum itaque provident, voluptas iusto harum eaque rem quam illo recusandae, ratione! Et corrupti a iste labore alias!</p></p> 	')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
